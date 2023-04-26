4b1dc63f243d0074633631782e4f8299088ec3e3
eunseo <eundeok9@gmail.com>
2023-04-26 14:41:56 +0900

    실습1번 작성

---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+const Counter = () => {
+  // useState를 사용하여 +1, -1 버튼을 눌렀을 때 값이 증가/감소 하는 것이 나타나도록 코드를 수정해주세요!
+  let count = 0;
+
+  const handleDecrease = () => {
+    count = count - 1;
+  };
+
+  const handleIncrease = () => {
+    count = count + 1;
+  };
+
+  return (
+    <div className="counter">
+      <p>{count}</p>
+      <button onClick={handleDecrease}>-1</button>
+      <button onClick={handleIncrease}>+1</button>
+    </div>
+  );
+};
+
+export default Counter;
