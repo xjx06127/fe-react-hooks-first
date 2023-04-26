5214e319532ff9382235151dc39f1fa416a08035
eunseo <eundeok9@gmail.com>
2023-04-26 14:41:00 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./components/Counter";
+import Name from "./components/Name";
+import Diary from "./components/Diary";
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <Name />
+      <Diary />
+    </>
+  );
+}
+
+export default App;
