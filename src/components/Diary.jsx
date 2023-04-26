e535d510a8c67d465130b4c8b354f7e2dc3d461b
HongseokChoi <xjx06127@inha.edu>
2023-04-26 20:56:08 +0900

    도전과제 제외 완료

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -1,19 +1,95 @@
-import React, {useState} from "react";
+//일반 과제
+
+/*import React, {useState, useTransition} from "react";
 
 const Diary = () => {
   // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
   // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
   // 실습으로 다뤘던 두 문제를 참고하면 충분히 해결할 수 있으실거라 생각합니다!
-
+  const[subject, setSub] = useState("");
+  const[content, setCon] = useState("");
+  
+  const handleSub = (e) => {
+    setSub(e.target.value);
+  }
+  const handleCon = (e) => {
+    setCon(e.target.value);
+  }
+  const reset = () => {
+    setSub("");
+    setCon("");
+  }
+  const submit = () => {
+    alert("제목: " + subject + "\n" + "내용: " + content);
+  }
   return (
     <div className="diary">
-      <input name="subject" placeholder="제목을 입력해주세요" />
+      <input value={subject} name="subject" placeholder="제목을 입력해주세요" onChange={handleSub}/> 
       <br />
-      <textarea name="content" placeholder="내용을 입력해주세요" />
+      <textarea value={content} name="content" placeholder="내용을 입력해주세요" onChange={handleCon}/>
       <br />
-      <button>초기화</button>
+      <button onClick={reset}>초기화</button>
       <br />
-      <button>입력 완료</button>
+      <button onClick={submit}>입력 완료</button>
+    </div>
+  );
+}; 
+
+export default Diary;
+
+*/
+
+
+//도전과제
+import React, {useState, useTransition} from "react";
+
+const Diary = () => {
+  // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
+  // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
+  // 실습으로 다뤘던 두 문제를 참고하면 충분히 해결할 수 있으실거라 생각합니다!
+  const [info, setInfo] = useState({
+    subject: "",
+    content: ""
+  });
+
+  function handleChanger(e) {
+    const {name, value} = e.target;
+
+    setInfo((prevValue) => {
+      if(name === "subject") {
+        return{
+          subject:value,
+          content:prevValue.content
+        }
+      } else if(name==="content") {
+        return {
+          subject:prevValue.subject,
+          content:value
+        }
+      }
+    })
+    
+  }
+  const reset = () => {
+    setInfo({
+      info: {
+        subject:"asdf",
+        content:"asdf"
+      },
+    });
+  }
+  const submit = () => {
+    alert("제목: " + info.subject + "\n" + "내용: " + info.content);
+  }
+  return (
+    <div className="diary">
+      <input value={info.subject} name="subject" placeholder="제목을 입력해주세요" onChange={handleChanger}/> 
+      <br />
+      <textarea value={info.content} name="content" placeholder="내용을 입력해주세요" onChange={handleChanger}/>
+      <br />
+      <button onClick={reset}>초기화</button>
+      <br />
+      <button onClick={submit}>입력 완료</button>
     </div>
   );
 };
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,21 +2,26 @@ import React, {useState} from "react";
 
 const Name = () => {
   // 이곳에 useState 구문을 작성해주세요
+  const[myName, setMyName] = useState("");
+  const[nickName, setNickName] = useState("");
 
-  const handleName = () => {
+
+  const handleName = (e) => {
     // 이곳에 이름을 설정하도록 하는 코드를 작성해주세요
+    setMyName(e.target.value);
   };
 
-  const handleNickName = () => {
+  const handleNickName = (e) => {
     // 이곳에 닉네임을 설정하도록 하는 코드를 작성해주세요
+    setNickName(e.target.value)
   };
 
   return (
     <div className="name">
       <input name="myName" placeholder="이름을 입력해주세요" onChange={handleName} />
       <input name="nickName" placeholder="닉네임을 입력해주세요" onChange={handleNickName} />
-      <p>이름: {/* 여기 중괄호 안에 닉네임이 표시되도록 하는 코드를 작성해주세요 */}</p>
-      <p>닉네임: {/* 여기 중괄호 안에 닉네임이 표시되도록 하는 코드를 작성해주세요 */}</p>
+      <p>이름: {myName==="" ? "아직 이름이 없습니다." : myName}</p>
+      <p>닉네임: {nickName==="" ? "아직 닉네임이 없습니다." : nickName}</p>
     </div>
   );
 };
